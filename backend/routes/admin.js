34c1ee1a1680bc15eb8f9def600d61a662c0783d
Ahmed Hesham <ahmedhesham2015fcih@gmail.com>
2023-04-15 01:29:49 +0200

    after add validation middleware for create a customer

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import Auth from "../handlers/admin/Auth.js"
 import Customer from '../handlers/admin/Customer.js'
+import CreateCustomer from '../validations/admin/customers/CreateCustomer.js'
 
 const routes = express.Router()
 
 routes.post("/login", Auth.login)
 
 routes.get("/customers", Customer.getCustomers)
-routes.post("/customers", Customer.create)
+routes.post("/customers", CreateCustomer, Customer.create)
 routes.get("/customers/:id", Customer.show)
 routes.put("/customers/:id", Customer.update)
 routes.delete("/customers/:id", Customer.destroy)
