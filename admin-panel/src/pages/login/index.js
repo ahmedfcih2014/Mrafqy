84804ab92c477d401101447cfa2e3331c3607bd7
Ahmed Hesham <ahmedhesham2015fcih@gmail.com>
2023-04-25 18:06:12 +0200

    add error labels to admin login form

--- admin-panel/src/pages/login/index.js
+++ admin-panel/src/pages/login/index.js
@@ -1,8 +1,35 @@
 import { useState } from "react"
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [errors ,setErrors] = useState({username: "", password: ""})
+
+    const changeUsername = e => {
+        setErrors({...errors, username: ""})
+        setUsername(e.target.value)
+    }
+
+    const changePassword = e => {
+        setErrors({...errors, password: ""})
+        setPassword(e.target.value)
+    }
+
+    const submitLogin = () => {
+        if (!username) {
+            setErrors({...errors, username: "Username can`t be empty"})
+            return
+        }
+        if (!password) {
+            setErrors({...errors, password: "Password can`t be empty"})
+            return
+        }
+        console.log({
+            username,
+            password
+        })
+    }
+
     return (
         <div className="flex flex-col gap-0 margin-20-perc p-6 rounded-xl mt-8" style={{backgroundColor: "white"}}>
             <div className="mb-6">
@@ -13,8 +40,15 @@ const Login = () => {
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                         placeholder="Username"
                         value={username}
-                        onChange={e => setUsername(e.target.value)}
+                        onChange={changeUsername}
                     />
+                    {
+                        errors.username ? 
+                        <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                            { errors.username }
+                        </span>
+                        : null
+                    }
                 </div>
             </div>
             <div className="mb-6">
@@ -25,12 +59,20 @@ const Login = () => {
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                         placeholder="Password"
                         value={password}
-                        onChange={e => setPassword(e.target.value)}
+                        onChange={changePassword}
                     />
+                    {
+                        errors.password ? 
+                        <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                            { errors.password }
+                        </span>
+                        : null
+                    }
                 </div>
             </div>
             <button
                 type="submit"
+                onClick={submitLogin}
                 className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                 Login
             </button>
