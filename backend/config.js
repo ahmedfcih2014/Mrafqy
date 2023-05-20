f4400016e2c9a26dc08d55ca5fd4d6c14f8eb090
Ahmed Hesham <ahmedhesham2015fcih@gmail.com>
2023-05-20 14:07:58 +0300

    enhancement in collection & add postgres cred

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,12 @@
 export default {
     encryptionSecret: "mrafqy-secret",
     db: {
-        user: 'ahmed',
-        host: 'localhost',
-        database: 'mrafqy',
-        password: 'ahmed',
+        user: 'ahmedhesham2020bb',
+        host: 'ep-old-boat-247923.eu-central-1.aws.neon.tech',
+        database: 'neondb',
+        password: 'Atj9yqfgEiH7',
         port: 5432,
+        ssl: true
     },
     adminTokenTime: 120,// minutes
     vatPercentage: 14,
